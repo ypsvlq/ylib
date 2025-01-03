f728f6573dcf7d7f9131731c6c0db8811c60c94f
Elaine Gibson <ypsvlq@gmail.com>
2025-01-03 11:13:27 +0000

    Ini: simplify unpack interface

--- src/Ini.zig
+++ src/Ini.zig
@@ -56,34 +56,31 @@ pub const Entry = struct {
         whitelist: []const []const u8 = &.{},
     };
 
-    pub fn unpack(self: Entry, allocator: ?std.mem.Allocator, comptime T: type, dest: anytype, comptime options: UnpackOptions) !void {
-        const info = @typeInfo(T).@"struct";
-        const fields = if (@TypeOf(dest) == *T) info.fields else info.decls;
+    pub fn unpack(self: Entry, dest: anytype, comptime options: UnpackOptions) !void {
+        const fields = if (@TypeOf(dest) == type) @typeInfo(dest).@"struct".decls else @typeInfo(@TypeOf(dest.*)).@"struct".fields;
 
         inline for (fields) |field| blk: {
             const ptr = &@field(dest, field.name);
             const FieldType = @TypeOf(ptr.*);
 
-            comptime {
-                switch (@typeInfo(FieldType)) {
-                    .@"fn", .type => break :blk,
-                    else => {},
-                }
+            switch (@typeInfo(FieldType)) {
+                .@"fn", .type => break :blk,
+                else => {},
+            }
 
-                for (options.blacklist) |item| {
-                    if (std.mem.eql(u8, item, field.name)) break :blk;
-                }
+            for (options.blacklist) |item| {
+                if (std.mem.eql(u8, item, field.name)) break :blk;
+            }
 
-                if (options.whitelist.len > 0) ok: {
-                    for (options.whitelist) |item| {
-                        if (std.mem.eql(u8, item, field.name)) break :ok;
-                    }
-                    break :blk;
+            if (options.whitelist.len > 0) ok: {
+                for (options.whitelist) |item| {
+                    if (std.mem.eql(u8, item, field.name)) break :ok;
                 }
+                break :blk;
             }
 
             if (std.mem.eql(u8, field.name, self.key)) {
-                return set(allocator, FieldType, ptr, self.value.?);
+                return set(FieldType, ptr, self.value.?);
             }
         }
 
@@ -91,9 +88,9 @@ pub const Entry = struct {
     }
 
     // ptr is anytype to allow *T and *?T
-    fn set(allocator: ?std.mem.Allocator, comptime T: type, ptr: anytype, value: []const u8) !void {
+    fn set(comptime T: type, ptr: anytype, value: []const u8) !void {
         if (T == []const u8) {
-            ptr.* = if (allocator) |ally| try ally.dupe(u8, value) else value;
+            ptr.* = value;
             return;
         }
 
@@ -102,12 +99,12 @@ pub const Entry = struct {
             .float => ptr.* = try std.fmt.parseFloat(T, value),
             .bool => ptr.* = if (std.mem.eql(u8, value, "true")) true else if (std.mem.eql(u8, value, "false")) false else return error.InvalidBool,
             .@"enum" => ptr.* = std.meta.stringToEnum(T, value) orelse return error.InvalidEnum,
-            .optional => return set(allocator, std.meta.Child(T), ptr, value),
+            .optional => return set(std.meta.Child(T), ptr, value),
             .array => {
                 var iter = std.mem.tokenizeScalar(u8, value, ' ');
                 for (ptr) |*elem_ptr| {
                     const elem = iter.next() orelse return error.NotEnoughElements;
-                    try set(allocator, std.meta.Child(T), elem_ptr, elem);
+                    try set(std.meta.Child(T), elem_ptr, elem);
                 }
                 if (iter.next() != null) return error.ExtraElement;
             },
